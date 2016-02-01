cc73c1a63936826cba9f540b3f3b13076b4feeb7
anennya <anennya@Anennyas-Laptop.local>
2016-01-31 19:02:46 -0800

    Given tweet ids this says those tweets

--- Kili/TextToSpeechViewController.swift
+++ Kili/TextToSpeechViewController.swift
@@ -8,16 +8,49 @@
 
 import UIKit
 import AVFoundation
+import TwitterKit
 
 
-class TextToSpeechViewController: UIViewController {
+class TextToSpeechViewController: UIViewController,TWTRTweetViewDelegate {
     let synth = AVSpeechSynthesizer()
     var myUtterance = AVSpeechUtterance(string: "")
-    
+    var tweets: [String] = []
+    var tweetIDs = ["266031293945503744", "440322224407314432"]
     @IBOutlet weak var textView: UITextView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        Twitter.sharedInstance().logInWithCompletion { session, error in
+            // Check we have a valid guest session.
+            if session != nil {
+                let client = TWTRAPIClient()
+                client.loadTweetsWithIDs(self.tweetIDs) { twttrs, error in
+                //client.loadUserWithID("@anennya") { twttrs, error in
+                    
+                    // If there are tweets do something magical
+                    if ((twttrs) != nil) {
+                        
+                        // Loop through tweets and do something
+                        for i in twttrs! {
+                            let tweet = i as! TWTRTweet
+                        
+                            self.tweets.append(tweet.text)
+                            
+                            }
+                        
+                        for i in self.tweets {
+                            print(i)
+                        }
+                            //print(self.tweets[0].text)
+                        }
+                     else {
+                        print(error)
+                    }
+                }
+                
+            }
+        }
+
 
         // Do any additional setup after loading the view.
     }
@@ -28,9 +61,21 @@ class TextToSpeechViewController: UIViewController {
     }
     
     @IBAction func KiliTalking(sender: UIButton) {
-        myUtterance = AVSpeechUtterance(string: textView.text)
-        myUtterance.rate = 0.5
-        synth.speakUtterance(myUtterance)
+        var actualText = ""
+       // var toSpeak = ""
+        
+        for tweet in self.tweets{
+            var myArray : [String] = tweet.componentsSeparatedByString("http")
+            print(myArray)
+            actualText = myArray[0]
+            myUtterance = AVSpeechUtterance(string: actualText)
+            myUtterance.rate = 0.5
+            synth.speakUtterance(myUtterance)
+
+        }
+        
+        
+
     }
 
     /*
